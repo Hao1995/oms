b311ef80d2bac71788499d41739fce1863086bff
Harry Lu <harry.nan.825@gmail.com>
2025-02-20 17:27:04 +0800

    feat: add campaign model

---
+++ app/models/campaign.rb
@@ -0,0 +1,7 @@
+class Campaign < ApplicationRecord
+  validates :customer_id, presence: true
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :currency, inclusion: { in: %w[USD TWD] }
+  validates :budget, presence: true, numericality: { greater_than_or_equal_to: 0 }
+end
+  
