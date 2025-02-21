6bbe4750925698382ec9a3c8839a0b8b8404f5ef
Harry Lu <harry.nan.825@gmail.com>
2025-02-21 10:04:48 +0800

    feat: add crud API

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,13 +1,22 @@
 module Api
   module V1
     class CampaignsController < ApplicationController
+      before_action :set_campaign, only: [:show, :update, :destroy]
+
       CUSTOMER_ID = 123
+
+      # GET /campaigns
       def index
+        @campaigns = Campaign.all
+        render json: @campaigns
       end
 
+      # GET /campaigns/:id
       def show
+        render json: @campaign
       end
 
+      # POST /campaigns
       def create
         @campaign = Campaign.new(campaign_params)
         if @campaign.save
@@ -17,13 +26,27 @@ module Api
         end
       end
 
+      # PUT /campaigns/:id
       def update
+        if @campaign.update(campaign_params)
+          render json: @campaign
+        else
+          render json: @campaign.errors, status: :internal_server_error
+        end
       end
 
+      # DELETE /campaigns/:id
       def destroy
+        @campaign.destroy
+        head :no_content
       end
 
       private
+
+      def set_campaign
+        @campaign = Campaign.find_by(id: params[:id])
+        render json: { error: 'Campaign not found' }, status: :not_found unless @campaign
+      end
 
       def campaign_params
         params.permit(:title, :currency, :budget, :advertiser_id)
